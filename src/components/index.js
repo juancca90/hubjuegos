65523e6dcce3d24540cb12e817241e1355c7cad0
juancca90 <castanedacamilo819@gmail.com>
2024-01-27 12:11:29 -0500

    ComponentesSpinner

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export * from "./Header/Header";
 export * from "./Footer/Footer";
-export * from "./CardPokemons/CardPokemons"
+export * from "./CardPokemons/CardPokemons";
+export * from "./spinner/Spinner";
+export * from "./spinner/SpinnerButton";
