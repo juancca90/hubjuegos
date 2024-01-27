9f128d791c95305a8d7dc43737ced9f5fb7a4cbd
juancca90 <castanedacamilo819@gmail.com>
2024-01-27 14:14:21 -0500

    TraerDatosPokeAPI

--- src/services/pokemon.service.js
+++ src/services/pokemon.service.js
@@ -1,9 +1,10 @@
 import { axiosUtil } from "../utils";
 
-export const getByPokemon = async (id) => {
+export const getByIdPokemon = async (id) => {
     const optionsRequest = {
         method: "GET",
         url: `https://pokeapi.co/api/v2/pokemon/${id}`,
     };
     return await axiosUtil(optionsRequest);
 };
+
---
+++ src/utils/dataPokemon.js
@@ -0,0 +1,75 @@
+import { setData } from "../global/state/globalState";
+import { getByIdPokemon } from "../services/pokemon.service";
+import { Paginacion } from "./paginacion";
+import { typePokemon } from "./typePokemon";
+
+let dataGlobal;
+export const dataPokemon = async () => {
+
+    const rawData = [];
+
+    for (let i = 1; i < 151; i++) {
+        rawData.push(await getByIdPokemon(i));
+    }
+    return dataMap(rawData);
+};
+
+const dataMap = (data) => {
+    const filterData = data.map((pokemon) => ({
+        name: pokemon.name,
+        image: pokemon.sprites.other.dream_world.front_default,
+        type: pokemon.types,
+        id: pokemon.id,
+    }));
+    return dataGlobal;
+};
+
+export const filterPokemon = (filterDataInputButton, donde) => {
+    switch (donde) {
+        case "type":
+            {
+                const filterData = dataGlobal.pokemonData.filter((pokemon) =>
+                pokemon.type[0].type.name
+                .toLowerCase()
+                .includes(filterDataInputButton.toLowerCase())
+                );
+
+                if (filterData.length === 0) {
+                    const filterData = dataGlobal.pokemonData.filter((pokemon) =>
+                        pokemon.type[1]?.type.name
+                        .toLowerCase()
+                        .includes(filterDataInputButton.toLowerCase())
+                    );
+
+                    Paginacion(filterData, 3);
+                } else {
+                    Paginacion(filterData, 3);
+                }
+            }
+
+            break;
+
+        case "name":
+            {
+                const filterData = dataGlobal.pokemonData.filter((pokemon) =>
+                pokemon.name
+                .toLowerCase()
+                .includes(filterDataInputButton.toLowerCase())
+                );
+            if (filterDataInputButton == "") {
+                Paginacion(filterData, 25);
+            } else {
+                Paginacion(filterData, 5);
+            }
+            }
+            break;
+    }
+};
+
+export const getInfo = async () => {
+    console.log("actualizando info... 🕵🏽‍♂️");
+    const data = await dataPokemon();
+    setData(data, "Pokemon");
+};
+
+getInfo();
